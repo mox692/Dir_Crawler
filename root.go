7a8fc7aabbd14edd8bb63bd03ec5d683d63bceec
mox692 <55653825+mox692@users.noreply.github.com>
2020-10-30 21:23:57 +0900

    find localfile

--- root.go
+++ root.go
@@ -2,18 +2,58 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"golang.org/x/xerrors"
 )
 
-func walk() {
+func walk(input string) string {
+	var fileName string = ""
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		fmt.Printf("visited file or dir: %q\n", path)
+		if info.IsDir() {
+			if strings.Contains(info.Name(), ".git") {
+				return filepath.SkipDir
+			}
+		}
+
+		if !(info.IsDir()) {
+			file, err := os.Open(path)
+			if err != nil {
+				return xerrors.Errorf("err: %w", err)
+			}
+			buf, err := ioutil.ReadAll(file)
+			if err != nil {
+				return xerrors.Errorf("err: %w", err)
+			}
+
+			// fmt.Println("テキストの中身：", string(buf))
+			if strings.Contains(string(buf), input) {
+				fileName = info.Name()
+			}
+
+			file.Close()
+		}
+		// fmt.Printf("visited file or dir: %v\n", path)
 		return nil
 	})
-	fmt.Println(err)
+	if err != nil {
+		log.Fatal("err: %w", err)
+	}
+	return fileName
 }
 
 func main() {
-	walk()
+	input := os.Args[1]
+	result := walk(input)
+
+	if result == "" {
+		fmt.Printf("fileが見つかりません。")
+	} else {
+		fmt.Printf("file: %s", result)
+	}
+
 }
